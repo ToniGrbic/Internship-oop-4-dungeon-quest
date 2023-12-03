45a0a7107101fda46c9b3f02b29d9054138af2fa
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-03 01:59:52 +0100

    added new hero methods and fixes

--- Domain/Repositories/Hero.cs
+++ Domain/Repositories/Hero.cs
@@ -17,15 +17,26 @@ public class Hero
         XPTreshold = 100;
         Trait = "";
     }
-    public void GainXP(int gainedXP)
+    public void GainExperienceAndLevelUp(int gainedXP)
     {
         if(XP + gainedXP >= XPTreshold)
         {
-            Level++;
+            ++Level;
+            Console.WriteLine(
+                "YOU HAVE LEVELED UP!!\n" +
+                $"NEW LVL: {Level}\n" +
+                $"***************************"
+            );
             XP = (XP + gainedXP) - XPTreshold;
-            //XPTreshold += 100; poslje mozda dodat da se threshold povecava sa levelUp
+            HPTheshold += 50;
             HP += 100;
-            Damage += 10;
+            Damage += 30;
+
+            if (this is Enchanter enchanter)
+            {
+                enchanter.ManaThreshold += 50;
+                enchanter.Mana = enchanter.ManaThreshold;
+            }
         }
         else
             XP += gainedXP;
@@ -33,17 +44,54 @@ public class Hero
     public void BasicAttack(Enemy enemy)
     {
         enemy.HP -= Damage;
+        if (this is Enchanter enchanter)
+        {
+            enchanter.Mana -= 15;
+        }
     }
     public void SpendXPforHP(int amount)
     {
-        if(XP < 100 && XP > amount)
+        if(XP < 100 && XP >= amount)
         {
             XP -= amount;
             HP += amount;
+            Console.WriteLine(
+               $"\n{amount} HP added.\n" +
+               $"HEALTH: {HP}\n" +
+               $"EXPERIENCE: {XP}\n"
+           );
         }
         else{
-            Console.WriteLine("Not enough XP");
+            Console.WriteLine("Not enough XP\n");
         }
+    }
+
+    public void RegainHealthAfterBattle()
+    {
+        var HPToGain = (int)(HPTheshold * 0.25);
+        if (HP + HPToGain > HPTheshold)
+        {
+            HP = HPTheshold;
+            Console.WriteLine($"Healed to MAX HP\n");
+        }
+        else
+        {
+            HP += HPToGain;
+            Console.WriteLine($"Healed for: {HPToGain}\n");
+        }
+        this.PrintHeroStats();
+
+    }
+    public void PrintHeroStats()
+    {
+        Console.WriteLine(
+                $"HERO: {Name}\n" +
+                $"Trait: {Trait}\n" +
+                $"Level: {Level}\n" +
+                $"HP: {HP} / {HPTheshold}\n" +
+                $"XP: {XP} / {XPTreshold}\n" +
+                $"Damage: {Damage}"
+        );
     }
 }
 
