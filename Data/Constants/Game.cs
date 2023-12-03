dee01f5cff131938228144cc8d3db1825922fe32
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-03 01:54:50 +0100

    renamed file and added new enums

---
+++ Data/Constants/Game.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Data.Constants
+{
+    public enum GameLoop
+    {
+        CONTINUE,
+        EXIT
+    }
+    public enum GameState
+    {
+        IN_PROGRESS,    
+        WIN,
+        LOSS,
+    }
+
+    public enum AttackType
+    {
+        DIRECT,
+        SIDE,
+        COUNTER
+    }
+
+    public enum CombatOutcome
+    {
+        WIN,
+        LOSE,
+        DRAW
+    }
+}
