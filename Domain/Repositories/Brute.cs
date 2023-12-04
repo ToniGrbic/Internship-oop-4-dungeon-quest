e0f5fa6ddb241f59c891bd18a4fa3d3d53ed684b
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-04 20:06:22 +0100

    implemented Brute heavy attack

--- Domain/Repositories/Brute.cs
+++ Domain/Repositories/Brute.cs
@@ -5,6 +5,9 @@ namespace Domain.Repositories
 {
     public class Brute:Enemy
     {
+        public bool isHeavyStrike = false;
+        public float HeavyStrikeChance = 0.30f;
+        public float PercentageOfHPDamage = 0.20f;
         public Brute()
         {
             this.HPThreshold = (int)EnemiesHP.Brute;
@@ -12,6 +15,31 @@ namespace Domain.Repositories
             this.XP = (int)EnemiesXP.Brute;
             this.Damage = (int)EnemiesDamage.Brute;
             this.Type = "Brute";
+        }
+
+        public override void BasicAttack(Hero hero)
+        {
+            var random = new Random();
+            var choice = random.Next(1, 101);
+            var HeavyStrikeOdds = HeavyStrikeChance * 100;
+            if (choice <= HeavyStrikeOdds)
+                isHeavyStrike = true;
+
+            if (isHeavyStrike)
+            {
+                var HeavyStrikeDamage = (int)(hero.HPThreshold * PercentageOfHPDamage);
+                hero.HP -= HeavyStrikeDamage;
+                Console.WriteLine(
+                    $"Enemy {Type} made a Heavy Strike!\n" +
+                    $"Hero is damaged for {HeavyStrikeDamage}");
+                isHeavyStrike = false;
+            }
+            else
+            {
+                hero.HP -= Damage;
+                Console.WriteLine($"Enemy {Type} has damaged you for {Damage}");
+            }
+                
         }
     }
 }
