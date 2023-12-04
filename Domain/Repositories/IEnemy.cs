ac4fb14059213855d37296f4a8dea8a519bdcce5
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-04 20:02:37 +0100

    added interfaces for Hero and Enemy classes

---
+++ Domain/Repositories/IEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface IEnemy
+    {
+        public int HP { get; set; }
+        public int HPThreshold { get; set; }
+        public int XP { get; set; }
+        public int Damage { get; set; }
+        public string Type { get; set; }
+        public bool IsStunned { get; set; }
+        public virtual void BasicAttack(Hero hero) { }
+        public void PrintEnemyStats();
+    }
+}
---
+++ Domain/Repositories/IHero.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Domain.Repositories
+{
+    public interface IHero
+    {
+        public string Name { get; set; }
+        public int XP { get; set; }
+        public int XPThreshold { get; set; }
+        public int HPThreshold { get; set; }
+        public int Damage { get; set; }
+        public int HP { get; set; }
+        public int Level { get; set; }
+        public string? Trait { get; set; }
+        public virtual void BasicAttack(Enemy enemy) { }
+        public virtual void UseHeroAbility() { }
+    }
+}
