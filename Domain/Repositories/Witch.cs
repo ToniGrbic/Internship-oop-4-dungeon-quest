f6689b83c8ad86cf79fe4c2ee8b446ac30b6f0ce
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-05 19:22:38 +0100

    implemented Witch ability

--- Domain/Repositories/Witch.cs
+++ Domain/Repositories/Witch.cs
@@ -5,19 +5,46 @@ namespace Domain.Repositories
     public class Witch:Enemy
     {
         public List<Enemy> EnemiesToSpawn { get; set; } = new();
-
+        public int DisarrayHealthPercentage { get; set; }
         public Witch()
         {
             this.HPThreshold = (int)EnemiesHP.Witch;
             this.HP = HPThreshold;
             this.XP = (int)EnemiesXP.Witch;
             this.Damage = (int)EnemiesDamage.Witch;
-            this.Type = "Witch";    
+            this.Type = "Witch";
+            this.DisarrayHealthPercentage = (new Random().Next(20, 71));
         }
-
         public void DisarrayAbility(Hero hero)
         {
+            Console.WriteLine(
+                  $"The {Type} has used Disarray Ability!\n" +
+                  $"Everyones HP is set to {DisarrayHealthPercentage}% !\n"
+            );
             
+            hero.HP = hero.HPThreshold * (int)(DisarrayHealthPercentage /100f);
+            Console.WriteLine($"Hero HP: {hero.HP} / {hero.HPThreshold}");
+
+            this.HP = HPThreshold * (int)(DisarrayHealthPercentage /100f);
+            Console.WriteLine($"Enemy {Type} HP: {HP} / {HPThreshold}");
+
+            foreach (var enemy in EnemiesToSpawn){
+                enemy.HP = enemy.HPThreshold * (int)(DisarrayHealthPercentage / 100f);
+                Console.WriteLine($"Enemy {enemy.Type} HP: {enemy.HP} / {enemy.HPThreshold}");  
+            }
+               
+        }
+
+        public override void BasicAttack(Hero hero)
+        {
+            var disarryChance = new Random().Next(1, 101);
+            if(disarryChance <= 50){
+                DisarrayAbility(hero);
+            }
+            else{
+                hero.HP -= Damage;
+                Console.WriteLine($"Enemy {Type} has damaged you for {Damage}");
+            }
         }
     
     }
